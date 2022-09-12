bcf157d73eaa473e679494aff605277c7c95700f
JpSymanski <70335886+JpSymanski@users.noreply.github.com>
2022-09-11 21:18:44 -0500

    cleaned up all tests got rid of all warnings.

--- java/src/test/java/CalcTest.java
+++ java/src/test/java/CalcTest.java
@@ -1,67 +1,65 @@
-import UI.Application;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import main.ExpressionSolver;
 import UI.DisplayHelper;
-import UI.Application;
 public class CalcTest {
 
-    private ExpressionSolver calculate;
+
     private DisplayHelper Dis;
 
     @BeforeEach
     public void DisSetup() {Dis = new DisplayHelper();}
 
-    private void assertDisplayIs(String expected){
+    private void assertDisplayIs(){
         String actual = Dis.getValue();
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals("", actual);
     }
 
     //Quaternary Input, output should be base 10 for tests
-    private void assertSumIs(Integer expected){
-        Integer actual = calculate.sum("10","1");
-        Assertions.assertEquals(expected, actual);
+    private void assertSumIs(){
+        Integer actual = ExpressionSolver.sum("10","1");
+        Assertions.assertEquals((Integer) 5, actual);
     }
 
-    private void assertDiffIs(Integer expected){
-        Integer actual = calculate.subtraction("10","1");
-        Assertions.assertEquals(expected, actual);
+    private void assertDiffIs(){
+        Integer actual = ExpressionSolver.subtraction("10","1");
+        Assertions.assertEquals((Integer) 3, actual);
     }
 
-    private void assertMultIs(Integer expected){
-        Integer actual = calculate.multiply("10","1");
-        Assertions.assertEquals(expected, actual);
+    private void assertMultIs(){
+        Integer actual = ExpressionSolver.multiply("10","1");
+        Assertions.assertEquals((Integer) 4, actual);
     }
 
-    private void assertDivIs(Integer expected){
-        Integer actual = calculate.divide("20","1");
-        Assertions.assertEquals(expected, actual);
+    private void assertDivIs(){
+        Integer actual = ExpressionSolver.divide("20","1");
+        Assertions.assertEquals((Integer) 8, actual);
     }
 
     @Test
     public void testInitialValue_getDisplayIsEmpty()
     {
-        assertDisplayIs("");
+        assertDisplayIs();
     }
 
     @Test
     public void testSum(){
-       assertSumIs(5);
+       assertSumIs();
     }
 
     @Test
     public void testDiff(){
-        assertDiffIs(3);
+        assertDiffIs();
     }
 
     @Test
     public void testMult(){
-        assertMultIs(4);
+        assertMultIs();
     }
 
     @Test
     public void testDiv(){
-        assertDivIs(8);
+        assertDivIs();
     }
 }
